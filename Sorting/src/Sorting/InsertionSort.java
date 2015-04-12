4984354e03a0a1af0677e8dcc68776e178d5138d
jbrower95 <jbrower95@gmail.com>
2015-04-12 14:01:22 -0400

    implemented insertionsort

--- Sorting/src/Sorting/InsertionSort.java
+++ Sorting/src/Sorting/InsertionSort.java
@@ -7,9 +7,19 @@ public class InsertionSort {
 	 * @param unsortedArray		an unsorted array of integers
 	 * @return					the input array, sorted least to greatest
 	 */
-	public static int[] insertionSort(int[] unsortedArray) {
-		//TODO: S2 implements insertion sort
+	public static int[] insertionSort(int[] a) {
 		
+		for (int i = 1; i < a.length; i++) {
+			
+			int temp = a[i];
+			int j;
+			
+			for (j = i - 1; j >= 0 && temp < a[j]; j--) {
+				a[j+1] = a[j]
+			}
+			
+			a[j+1] = temp
+		}
 		return null;
 	}
 }
